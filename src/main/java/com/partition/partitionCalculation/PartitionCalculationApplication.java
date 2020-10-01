d387b30bf0a250c07f48429b366a2053a8c4d5a6
sofyane <sofyane@mail.com>
2020-10-02 00:41:37 +0200

    partition first commit

---
+++ src/main/java/com/partition/partitionCalculation/PartitionCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.partition.partitionCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartitionCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartitionCalculationApplication.class, args);
+	}
+
+}
